4fd71d3d6e87ec6d387290e8787b738aa9d60031
William Queiroz <wrq82@hotmail.com>
2019-05-05 22:05:00 -0300

    first commit

---
+++ src/Comment.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Comment = ({c}) => {
+    return(
+        <div>{c.comment}</div>
+    ) 
+}
+
+export default Comment
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+
+const config = {
+    apiKey: "AIzaSyABveoamKaAFZTcN45rbGs0uZkSo51sX8Q",
+    authDomain: "reactjs-746ec.firebaseapp.com",
+    databaseURL: "https://reactjs-746ec.firebaseio.com",
+    projectId: "reactjs-746ec",
+    storageBucket: "reactjs-746ec.appspot.com",
+    messagingSenderId: "993517826810"
+  }
+
+  firebase.initializeApp(config)
+
+  export const database = firebase.database()
+
+
+
