7cc90848fb9442a7fc5602c86595dd61d2ca230c
William Queiroz <wrq82@hotmail.com>
2019-05-07 20:05:05 -0300

    testes em Comments alterados

--- src/Comments.test.js
+++ src/Comments.test.js
@@ -5,13 +5,26 @@ import Comment from './Comment'
 
 describe('<Comments />', () =>{
     it('should render Comments', () => {
-        const comments = [
-            {id: 'a', comment: 'Comment 1'},
-            {id: 'b', comment: 'Comment 2'},
-            {id: 'c', comment: 'Comment 3'}
-        ]
+        const comments = {
+            a: {id: 'a', comment: 'Comment 1q'},
+            b: {id: 'b', comment: 'Comment 2'},
+            c: {id: 'c', comment: 'Comment 3'}
+        }
  
         const wrapper = shallow(<Comments comments = {comments} />)
         expect(wrapper.find(Comment).length).toBe(3)
+
+        expect(wrapper.find(Comment).get(0).props.c).toBe(comments.a)
+        expect(wrapper.find(Comment).get(1).props.c).toBe(comments.b)
+        expect(wrapper.find(Comment).get(2).props.c).toBe(comments.c)
+        //console.log(wrapper.find(Comment).get(0))
+        expect(wrapper.find(Comment).get(0).key).toBe(comments.a.id)
+        
+    })
+
+    it('should work with no Comments', () => {
+        const comments = {}
+        const wrapper  = shallow(<Comments comments = {comments} />)
+        expect(wrapper.find(Comment).length).toBe(0)
     })
 })
