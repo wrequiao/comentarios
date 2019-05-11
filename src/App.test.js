6b179951527504cf1e8c688a10d0ae6ef0fdd451
William Queiroz <wrq82@hotmail.com>
2019-05-11 19:16:09 -0300

    testando o app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import App from './App'
+import { shallow } from 'enzyme'
+import Comments from './Comments'
+import NewComment from './NewComment'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('<App />', () => {  
+  it('renders without crashing', () => {
+    const database = {
+      ref : jest.fn()
+    }
+
+    database.ref.mockReturnValue({
+      on : jest.fn()
+    })
+
+    const wrapper = shallow(<App database={database} />)
+    expect(wrapper.find(Comments).length).toBe(1)
+    expect(wrapper.find(NewComment).length).toBe(1)
+    expect(wrapper.find('p').length).toBe(1)
+  })
+
+
+  it('add a new Comment', () => {
+    
+    const database = {
+      ref : jest.fn()
+    }
+
+    const child = jest.fn()
+    const update = jest.fn()
+    database.ref.mockReturnValue({
+      on : jest.fn(),
+      child,
+      update
+    })
+
+    const push = jest.fn()
+    child.mockReturnValue({
+      push
+    })
+
+    push.mockReturnValue({
+      key : 1
+    })
+
+    const wrapper = shallow(<App database={database} />)
+    wrapper.instance().sendComment('new comment')
+    expect(child).toBeCalledWith('comments')
+    expect(update).toBeCalledWith({
+      'comments/1' : {
+        comment: 'new comment'
+      }
+    })
+
+  })
+
+})
+
+
